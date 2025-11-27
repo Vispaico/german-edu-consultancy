2907962a1742faf712287eb76361687e726c49dc
KinhCode <61858392+KinhCode@users.noreply.github.com>
2025-11-27 15:39:02 +0700

    Annoted the next.config.ts to enable image domains for headless CMS.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
-const nextConfig = {
+const nextConfig: import('next').NextConfig = {
   images: {
     remotePatterns: [
       {
